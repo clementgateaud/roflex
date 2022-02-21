03c81e36d506fccaf8155868c11006def7d63e21
Clement Gateaud <clement.gateaud@gmail.com>
2022-02-21 16:11:19 +0100

    added wishlist model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_150437) do
+ActiveRecord::Schema.define(version: 2022_02_21_151057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,18 @@ ActiveRecord::Schema.define(version: 2022_02_21_150437) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wishlists", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "offer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["offer_id"], name: "index_wishlists_on_offer_id"
+    t.index ["user_id"], name: "index_wishlists_on_user_id"
+  end
+
   add_foreign_key "offers", "users"
   add_foreign_key "rentals", "offers"
   add_foreign_key "rentals", "users"
+  add_foreign_key "wishlists", "offers"
+  add_foreign_key "wishlists", "users"
 end
