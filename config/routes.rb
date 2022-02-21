3ca4fb79f8482ded57e6d57dd8bb0b3af1eea3f5
Sacha Fontaine <sachafontaine92500@gmail.com>
2022-02-21 17:43:11 +0100

    push resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,8 @@
 Rails.application.routes.draw do
   get 'users/whishlist'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  get 'rentals/index'
-  get 'rentals/show'
-  get 'rentals/new'
-  get 'rentals/create'
-  get 'rentals/edit'
-  get 'rentals/update'
-  get 'rentals/destroy'
-  get 'offers/index'
-  get 'offers/show'
-  get 'offers/new'
-  get 'offers/create'
-  get 'offers/edit'
-  get 'offers/update'
-  get 'offers/destroy'
+  resources :users
+  resources :rentals
+  resources :offers
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
