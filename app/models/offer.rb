d6728f53dc48d9ff017803d285250ed81afc9908
Clement Gateaud <clement.gateaud@gmail.com>
2022-02-21 16:22:48 +0100

    added validations to models

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,9 @@
 class Offer < ApplicationRecord
   belongs_to :user
+  has_many :rentals
+  has_many :users, through: :rentals
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :photo_url, presence: true
+  validates :description, presence: true
 end
